3a87199662bc658f1e28122e7802c858213eebe6
felix <deelen@floodtags.com>
2018-08-08 17:35:07 +0200

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express     = require('express');
 var bodyParser  = require('body-parser')
+var {ObjectID}  = require('mongodb');
 
 var {mongoose}  = require('./db/mongoose');
 var {Todo}      = require('./models/todo');
@@ -25,9 +26,25 @@ app.get('/todos', (req, res) => { // return all todos in collection
   Todo.find().then( (todos) => {
     res.send({todos});
   }, (e) => {
-    res.status(400).send(e);
+    res.status(404).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send({}/*{message: 'Id is not valid'}*/);
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) return res.status(404).send({});
+    res.send({todo});
+  }).catch(e => {
+    res.status(400).send();
+  })
+
+
+})
 
 
 app.listen(3010, () => {
