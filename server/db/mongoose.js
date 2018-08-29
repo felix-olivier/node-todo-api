10db74f85945b8dc5f47181bc280ec014915b3a0
felix <deelen@floodtags.com>
2018-08-29 15:40:59 +0200

    Set up app for heroku (bugfix 2)

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // Tells mongoose which promise module to use
 
-url = process.env.PORT ? 'mongodb://felixolvier:1nodecourse@ds237192.mlab.com:37192/todoapp' : 'mongodb://localhost:27017/TodoApp';
+url = true /*process.env.PORT*/ ? 'mongodb://felixolivier:1nodecourse@ds237192.mlab.com:37192/todoapp' : 'mongodb://localhost:27017/TodoApp';
 console.log(url);
 mongoose.connect(url); // mongoose waits until connection is ready before performing any operations on it
 
